6e2512599355d8d5a06129f843394412efac4689
ashka777 <34421085+ashka777@users.noreply.github.com>
2020-09-24 16:09:03 +0300

    Add files via upload

---
+++ ВТП/ВТП/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ВТП
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(147, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(486, 268);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 181);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.DisplayMember = "1";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.listBox1.Location = new System.Drawing.Point(12, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(129, 121);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество строк";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество столбцов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 346);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Соломянюк Андрей_17ЗИЭ-1_Вариант 22. КЛИКНИТЕ ДВА РАЗА МЫШКОЙ";
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ВТП/ВТП/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ВТП
+{
+    public partial class Form1 : Form
+    {
+        int columns = 0;
+        int rows = 0;
+        int rand;
+        int for_window = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void DGW()
+        {
+            int i = 0;
+            int j = 0;
+            if (rows != 0 && columns != 0)
+            {
+                dataGridView1.ColumnCount = columns; // Столбцы
+                dataGridView1.RowCount = rows; // Строки
+                for (i = 0; i < columns; i++)
+                {
+                    for (j = 0; j < rows; j++)
+                    {
+                        Random_dgw();
+                        dataGridView1[i, j].Value = rand;
+                        dataGridView1.Columns[i].Width = 40;
+                    }
+                }
+            }
+            else
+            { return; }
+
+            int data_to_dgw = 0;// int data_to_dgw = -1;
+            dataGridView1.Rows.Add();
+            for (i = 0; i < columns; i++)
+            {
+                if (dataGridView1[i, j - 1].Value != null)
+                {
+                    for (j = 0; j < rows; j++)
+                    {
+                        int data = (int)dataGridView1[i, j].Value;
+                        if (data >=0) // if (data > data_to_dgw)
+                            data_to_dgw++; //data_to_dgw = data;
+                        if (for_window < data_to_dgw)
+                            for_window = data_to_dgw; // for_window = data;
+                    }
+                    
+                }
+                
+                if (data_to_dgw != -1)
+                {
+                    dataGridView1[i, j].Value = data_to_dgw;
+                    data_to_dgw = 0;
+                }
+            }
+        }
+
+        void Random_dgw()
+        {
+            Thread.Sleep(20);
+            Random randoms = new Random();
+            rand = randoms.Next(-100, 100);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for_window = 0;
+            rows = Convert.ToInt32(listBox1.SelectedItem);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for_window = 0;
+            columns = Convert.ToInt32(comboBox1.SelectedItem);
+
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if ((int)dataGridView1[i, rows].Value == for_window) //[i, j]
+                    {
+                        dataGridView1.Columns[i].DefaultCellStyle.BackColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            for (int i = 0; i < columns; i++)
+                dataGridView1.Columns[i].DefaultCellStyle.BackColor = Color.White;
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            for_window = 0;
+            rows = Convert.ToInt32(listBox1.SelectedItem);
+            columns = Convert.ToInt32(comboBox1.SelectedItem);
+            DGW();
+        }
+    }
+}
